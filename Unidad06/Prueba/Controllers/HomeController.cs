77012962440f7218a100ba92c94c0ad81bc62931
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-15 14:36:47 +0200

    [new[ ud07

---
+++ Unidad06/Prueba/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Prueba.Models;
+using System.Diagnostics;
+
+namespace Prueba.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public String Index()
+        {
+            return "Hola Marcos";
+        }
+
+        public int numerin()
+        {
+            return 33;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

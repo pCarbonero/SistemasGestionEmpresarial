26179b63476bc0b059dfb0c6147cbff816b16e00
pCarbonero <pablo.carbonero@iesnervion.es>
2024-10-16 19:55:49 +0200

    [new[ ej04

---
+++ Unidad06/Ejercicio04/Controllers/TablaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio04.Controllers
+{
+    public class TablaController : Controller
+    {
+        public IActionResult TablaMultiplicar()
+        {
+            return View();
+        }
+    }
+}

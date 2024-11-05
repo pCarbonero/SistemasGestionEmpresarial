c2ee95484c7ca37eb4c439f2b69109166c309a96
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-05 13:41:35 +0100

    [new] manda

---
+++ Ud08/EjercicioMandaloriano/EjercicioMandalorianoUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjercicioMandalorianoUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

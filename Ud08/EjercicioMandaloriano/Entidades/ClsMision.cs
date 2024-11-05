0928c4d6bb00347d41099a1d39acbf7c456bba9c
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-05 14:37:50 +0100

    [upd] cosas

---
+++ Ud08/EjercicioMandaloriano/Entidades/ClsMision.cs
@@ -0,0 +1,22 @@
+﻿namespace Entidades
+{
+    public class ClsMision
+    {
+        #region Propiedades
+        public int Id { get; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public float Recompensa { get; set; }
+        #endregion
+
+        #region Constructores
+        public ClsMision() { }
+        public ClsMision(string nombre, string descripcion, float recompensa)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Recompensa = recompensa;
+        }
+        #endregion
+    }
+}

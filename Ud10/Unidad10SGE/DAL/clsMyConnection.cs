f12e3149b96b68e3ef68fcaf7ff9775f8ffe8394
pCarbonero <pablo.carbonero@iesnervion.es>
2024-11-13 12:46:53 +0100

    zcfb

---
+++ Ud10/Unidad10SGE/DAL/clsMyConnection.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Data.SqlClient;
+namespace DAL
+{
+    public class clsMyConnection
+    {
+        //Atributos
+        public String server { get; set; }
+        public String dataBase { get; set; }
+        public String user { get; set; }
+        public String pass { get; set; }
+
+        //Constructores
+
+        public clsMyConnection()
+        {
+
+            this.server = "pCarboneroBD.database.windows.net";
+            this.dataBase = "pCarboneroBD";
+            this.user = "Usuario";
+            this.pass = "LaCampana123";
+
+        }
+        //Con parámetros por si quisiera cambiar las conexiones
+        public clsMyConnection(String server, String database, String user, String pass)
+        {
+            this.server = server;
+            this.dataBase = database;
+            this.user = user;
+            this.pass = pass;
+        }
+
+
+        //METODOS
+
+        /// <summary>
+        /// Método que abre una conexión con la base de datos
+        /// </summary>
+        /// <pre>Nada.</pre>
+        /// <returns>Una conexión abierta con la base de datos</returns>
+        public SqlConnection getConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+
+            try
+            {
+
+                connection.ConnectionString = $"server={server};database={dataBase};uid={user};pwd={pass};";
+                connection.Open();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return connection;
+
+        }
+
+
+
+
+        /// <summary>
+        /// Este metodo cierra una conexión con la Base de datos
+        /// </summary>
+        /// <post>La conexion es cerrada</post>
+        /// <param name="connection">SqlConnection pr referencia. Conexion a cerrar
+        /// </param>
+        public void closeConnection(ref SqlConnection connection)
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ Ud10/Unidad10SGE/Ejercicio01/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Ejercicio01.Models;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+
+
+namespace Ejercicio01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

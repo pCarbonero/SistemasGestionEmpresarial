7d977ca0df8f69c159effd0716cea0c1109d700d
pCarbonero <pablo.carbonero@iesnervion.es>
2025-01-16 14:37:11 +0100

    aa

---
+++ Unidad14/AngularHolaMundo/holaMundoAngular/src/app/components/formulario-persona/formulario-persona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-persona',
+  imports: [],
+  templateUrl: './formulario-persona.component.html',
+  styleUrl: './formulario-persona.component.css'
+})
+export class FormularioPersonaComponent {
+
+}

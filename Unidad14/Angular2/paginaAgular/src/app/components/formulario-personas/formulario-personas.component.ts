c304b5a5088e78ec3b471422c40d4cf9b2f3b355
pCarbonero <pablo.carbonero@iesnervion.es>
2025-01-23 14:35:28 +0100

    [upd] formGroup

--- Unidad14/Angular2/paginaAgular/src/app/components/formulario-personas/formulario-personas.component.ts
+++ Unidad14/Angular2/paginaAgular/src/app/components/formulario-personas/formulario-personas.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-personas',
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './formulario-personas.component.html',
   styleUrl: './formulario-personas.component.css'
 })
 export class FormularioPersonasComponent {
-
+  formulario!: FormGroup
+  ngOnInit() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl('',[Validators.required]),
+      apellidos: new FormControl('',[Validators.required]),
+    });
+  }
+  constructor() { }
+  saluda() {
+    if (this.formulario.valid) {
+      alert("Hola " + this.formulario.controls.nombre.value + " " + this.formulario.controls.apellidos.value); 
+    }
+  }
 }
